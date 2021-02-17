2dedcd19b3198fa1746343ea70f663954f114371
EC2 Default User <ec2-user@ip-172-31-19-16.us-east-2.compute.internal>
2021-02-17 10:13:54 +0000

    Laravel基礎課題４

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::orderBy('created_at', 'desc')->get();
+        return view('posts.index', ['posts' => $posts]);
+    }
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts')
+
+@section('content')
+
+    <div class="container mt-4">
+        @foreach($posts as $post)
+        <div class="card mb-4">
+            <div class="card-header mb-2">
+                {{$post->title}}
+            </div>
+            <div class="card-body">
+                <p class="card-text">
+                    {{$post->body}}
+                </p>
+            </div>
+            <div class="card-footer">
+                <span　class="mr-2">
+                    投稿日時
+                </span>
+            </div>
+        </div>
+        @endforeach
+    </div>
+    
+
+@endsection('content')
